39c2d777fbd8fe69c25c23be80a0172b2f110fa3
Christos Zoulas <christos@netbsd.org>
2015-01-18 17:28:37 +0000

    *** empty log message ***

--- include/bl.h
+++ include/bl.h
@@ -13,4 +13,5 @@ void bl_destroy(bl_t);
 int bl_send(bl_t, bl_type_t, int, int, const char *);
 int bl_recv(bl_t, bl_type_t *, int *, int *, char *, size_t);
 
-#define _PATH_BLOCKLIST "/tmp/blocklist"
+#define _PATH_BLSOCK "/tmp/blsock"
+#define _PATH_BLCONF "/etc/blocklist.conf"
