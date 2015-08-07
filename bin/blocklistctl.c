7fd14bfca81f062a648ca497623395f6102cc15c
Soren Jacobsen <snj@netbsd.org>
2015-08-07 04:10:23 +0000

    Pull up following revision(s) (requested by christos in ticket #922): 	external/bsd/blocklist/bin/blocklistctl.c: revision 1.18 	external/bsd/blocklist/bin/blocklistd.c: revision 1.33 	external/bsd/blocklist/bin/run.c: revision 1.13 	external/bsd/blocklist/bin/conf.c: revisions 1.21-1.23 	external/bsd/blocklist/bin/state.c: revisions 1.16, 1.17 	external/bsd/blocklist/bin/support.c: revision 1.7 	external/bsd/blocklist/bin/support.h: revision 1.6 	external/bsd/blocklist/etc/blocklistd.conf: revisions 1.5, 1.6 	external/bsd/blocklist/libexec/blocklistd-helper: revision 1.5 print the key in human readable form when debugging. -- Add more debugging, simplify. Use symbolic constants: -2=FEQUALS, -1=FSTAR -- make proto and port optional -- restore address length before printing. -- dissallow interface spec in remote config (since we check against the remote address, it does not make sense). -- remove interface example from remote config. -- simplify, comment out testing entries -- Restart the loop each time we delete an entry because the hash code does not handle it well. Is that a db bug?

--- bin/blocklistctl.c
+++ bin/blocklistctl.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: blocklistctl.c,v 1.17 2015/02/02 22:01:55 christos Exp $	*/
+/*	$NetBSD: blocklistctl.c,v 1.17.2.2 2015/04/30 06:07:33 riz Exp $	*/
 
 /*-
  * Copyright (c) 2015 The NetBSD Foundation, Inc.
@@ -33,7 +33,7 @@
 #endif
 
 #include <sys/cdefs.h>
-__RCSID("$NetBSD: blocklistctl.c,v 1.17 2015/02/02 22:01:55 christos Exp $");
+__RCSID("$NetBSD: blocklistctl.c,v 1.17.2.2 2015/04/30 06:07:33 riz Exp $");
 
 #include <stdio.h>
 #include <time.h>
@@ -85,7 +85,7 @@ main(int argc, char *argv[])
 	argc--;
 	argv++;
 
-	while ((o = getopt(argc, argv, "abdrw")) != -1)
+	while ((o = getopt(argc, argv, "abD:drw")) != -1)
 		switch (o) {
 		case 'a':
 			all = 1;
@@ -93,6 +93,9 @@ main(int argc, char *argv[])
 			break;
 		case 'b':
 			blocked = 1;
+		case 'D':
+			dbname = optarg;
+			break;
 			break;
 		case 'd':
 			debug++;
