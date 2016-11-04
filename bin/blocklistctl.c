73ccb732d3b00a39fab5234e861c1b77ed40eda5
Paul Goyette <pgoyette@netbsd.org>
2016-11-04 14:42:44 +0000

    Sync with HEAD

--- bin/blocklistctl.c
+++ bin/blocklistctl.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: blocklistctl.c,v 1.19 2015/11/04 16:21:52 christos Exp $	*/
+/*	$NetBSD: blocklistctl.c,v 1.21 2016/11/02 03:15:07 jnemeth Exp $	*/
 
 /*-
  * Copyright (c) 2015 The NetBSD Foundation, Inc.
@@ -33,7 +33,7 @@
 #endif
 
 #include <sys/cdefs.h>
-__RCSID("$NetBSD: blocklistctl.c,v 1.19 2015/11/04 16:21:52 christos Exp $");
+__RCSID("$NetBSD: blocklistctl.c,v 1.21 2016/11/02 03:15:07 jnemeth Exp $");
 
 #include <stdio.h>
 #include <time.h>
@@ -96,9 +96,9 @@ main(int argc, char *argv[])
 			break;
 		case 'b':
 			blocked = 1;
+			break;
 		case 'D':
 			dbname = optarg;
-			break;
 			break;
 		case 'd':
 			debug++;
