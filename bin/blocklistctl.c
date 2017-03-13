b813b0cbd092445ecdce2c44281111523a5bd9f4
Nick Hudson <skrll@netbsd.org>
2017-03-13 07:41:13 +0000

    Sync with netbsd-7-1-RELEASE

--- bin/blocklistctl.c
+++ bin/blocklistctl.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: blocklistctl.c,v 1.17.2.4 2016/12/18 07:58:52 snj Exp $	*/
+/*	$NetBSD: blocklistctl.c,v 1.17.2.3.4.1 2017/01/18 08:46:19 skrll Exp $	*/
 
 /*-
  * Copyright (c) 2015 The NetBSD Foundation, Inc.
@@ -33,7 +33,7 @@
 #endif
 
 #include <sys/cdefs.h>
-__RCSID("$NetBSD: blocklistctl.c,v 1.17.2.4 2016/12/18 07:58:52 snj Exp $");
+__RCSID("$NetBSD: blocklistctl.c,v 1.17.2.3.4.1 2017/01/18 08:46:19 skrll Exp $");
 
 #include <stdio.h>
 #include <time.h>
@@ -85,7 +85,7 @@ main(int argc, char *argv[])
 	argc--;
 	argv++;
 
-	while ((o = getopt(argc, argv, "abD:drw")) != -1)
+	while ((o = getopt(argc, argv, "abD:dnrw")) != -1)
 		switch (o) {
 		case 'a':
 			all = 1;
