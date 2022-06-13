zoulasc/blocklist

commit dfd697cf3433390d906bfd8d75910e6f902ee55d

Re-enable the routing perm check now that the kernel has been fixed to return valid information.

bin/conf.c
@@ -1,4 +1,4 @@
-/*	$NetBSD: conf.c,v 1.29 2020/03/12 19:35:11 christos Exp $	*/
+/*	$NetBSD: conf.c,v 1.30 2020/03/12 19:47:32 christos Exp $	*/
 
 /*-
  * Copyright (c) 2015 The NetBSD Foundation, Inc.
@@ -33,7 +33,7 @@
 #endif
 
 #include <sys/cdefs.h>
-__RCSID("$NetBSD: conf.c,v 1.29 2020/03/12 19:35:11 christos Exp $");
+__RCSID("$NetBSD: conf.c,v 1.30 2020/03/12 19:47:32 christos Exp $");
 
 #include <stdio.h>
 #ifdef HAVE_LIBUTIL_H
@@ -1006,7 +1006,7 @@ confset_match(const struct confset *cs, struct conf *c,
 static int
 conf_route_perm(int fd) {
 /* Disable for now, the access check in the routing socket uses curlwp */
-#if defined(RTM_IFANNOUNCE) && defined(RT_ROUNDUP) && 0
+#if defined(RTM_IFANNOUNCE) && defined(RT_ROUNDUP)
 	/*
 	 * Send a routing message that is not supported to check for access
 	 * We expect EOPNOTSUPP for having access, since we are sending a

commit 616f1c8b6f0591f4bd301c692f71a99d7b5426e9

conf.c: Remove stray comment
When reverting 4ca698e97e8d442dcd0eb24bd5b1b4207011f872, this comment
was missed.

Fixes:  dfd697cf3433390d906bfd8d75910e6f902ee55d

bin/conf.c
@@ -1005,7 +1005,6 @@ confset_match(const struct confset *cs, struct conf *c,
 #ifdef AF_ROUTE
 static int
 conf_route_perm(int fd) {
-/* Disable for now, the access check in the routing socket uses curlwp */
 #if defined(RTM_IFANNOUNCE) && defined(RT_ROUNDUP)
 	/*
 	 * Send a routing message that is not supported to check for access